2536371be736851f159b83767485acdf555eefab
matthewmiddlebrook-acu <mkm15a@acu.edu>
2018-12-06 11:46:01 -0600

    Added colors to forecast, similar to Weather Timeline app

--- lib/widgets/WeatherItem.dart
+++ lib/widgets/WeatherItem.dart
@@ -5,12 +5,26 @@ import 'package:intl/intl.dart';
 
 class WeatherItem extends StatelessWidget {
   final DailyForecastData weather;
+  final conditionColors = {
+    'clear-day': Colors.amber,
+    'clear-night': Colors.blueGrey,
+    'rain': Colors.indigo,
+    'snow': Colors.grey[100],
+    'sleet': Colors.blueGrey[200],
+    'wind': Colors.green,
+    'fog': Colors.grey[300],
+    'cloudy': Colors.grey,
+    'partly-cloudy-day': Color.lerp(Colors.blueGrey, Colors.amber, .75),
+    'partly-cloudy-night': Colors.blueGrey[600],
+  };
+  final textColor = Colors.white;
 
   WeatherItem({Key key, @required this.weather}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: conditionColors[weather.icon],
       child: Padding(
         padding: EdgeInsets.all(8.0),
         child: Row(
@@ -18,7 +32,7 @@ class WeatherItem extends StatelessWidget {
             Expanded(
               flex: 1,
               child: SvgPicture.asset("assets/${weather.icon}.svg",
-                  color: Colors.black, width: 54, height: 54),
+                  color: textColor, width: 54, height: 54),
             ),
             Expanded(
               flex: 6,
@@ -27,29 +41,26 @@ class WeatherItem extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
-                    Text(DateFormat.E().format(weather.date),
+                    Text(
+                      DateFormat.E().format(weather.date),
                       textAlign: TextAlign.start,
-                      style:
-                      TextStyle(color: Colors.black, fontSize: 24),
+                      style: TextStyle(color: textColor, fontSize: 24),
                     ),
                     Text(weather.main,
-                        style:
-                        TextStyle(color: Colors.black, fontSize: 16)),
+                        style: TextStyle(color: textColor, fontSize: 16)),
                   ],
                 ),
               ),
             ),
             Expanded(
-              flex: 2,
+              flex: 1,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text('${weather.tempH.round().toString()}°F',
-                      style:
-                      TextStyle(color: Colors.black, fontSize: 24)),
-                  Text('${weather.tempL.round().toString()}°F',
-                      style:
-                      TextStyle(color: Colors.black, fontSize: 24)),
+                  Text('${weather.tempH.round().toString()}°',
+                      style: TextStyle(color: textColor, fontSize: 24)),
+                  Text('${weather.tempL.round().toString()}°',
+                      style: TextStyle(color: textColor, fontSize: 24)),
                 ],
               ),
             ),
